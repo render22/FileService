8d7d1f9a1d959700c46ffe2c12b2b13b9c84577e
Viktar <vkantarovich@elinext.com>
2014-11-17 17:01:07 +0200

    changes 3

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 var im = require('imagemagick');
 im.identify('Desert.jpg', function(err, metadata){
-    if (err) throw err;
-    console.log(metadata);
+
 })
 
 
-im.resize({
-    srcPath: 'Desert.jpg',
-    dstPath: 'Desert-small.jpg',
-    width:   256
-}, function(err, stdout, stderr){
-    if (err) throw err;
-    console.log('resized kittens.jpg to fit within 256x256px');
-});
+
+
+
+var http=require('http'),
+    fileSystem = require('fs'),
+    path = require('path');
+http.createServer(function(req,response){
+    im.resize({
+        srcPath: 'Desert.jpg',
+        dstPath: 'Desert-small.jpg',
+        width:   256
+    }, function(err, stdout, stderr){
+        if (err) throw err;
+        console.log('resized kittens.jpg to fit within 256x256px');
+
+        var filePath = path.join(__dirname, 'Desert-small.jpg');
+        var stat = fileSystem.statSync(filePath);
+
+        response.writeHead(200, {
+            'Content-Type': 'image/jpeg',
+            'Content-Length': stat.size
+        });
+
+        var readStream = fileSystem.createReadStream(filePath);
+        // We replaced all the event handlers with a simple call to readStream.pipe()
+        readStream.pipe(response);
+        fileSystem.unlink(filePath);
+    });
+
+}).listen(3000);
